9917f3c58d388fca00f6363b5f93f0001925d841
Brian Longmore <brianl.ext@gmail.com>
2020-11-10 17:44:46 -1000

    added skeleton lines, they kinda funky sometimes tho

--- script.js
+++ script.js
@@ -23,7 +23,6 @@ function setup() {
 
 function draw() {
    setupCanvas();
-   
    drawPose(height);
 }
 
@@ -31,11 +30,13 @@ function draw() {
 
 // applies inital transformations and controls loading message 
 function setupCanvas() {
-   clear();
+   background(0);
+   noStroke();
 
    if (isLoading) {
       textAlign(CENTER, CENTER);
       textSize(30);
+      fill(255);
       text('Loading...', width/2, height/2);
    }
 
@@ -56,17 +57,54 @@ function setupPoseNet() {
 
 // draws image and points over it
 function drawPose(vidHeight) {
+   let minConfidence = 0.2;
    let imageScaleFactor = vidHeight/baseRes[1];
    
    translate(width/2 - calcRes(vidHeight)[0]/2, height/2 - vidHeight/2); // centers image and points
    image(video, 0, 0, ...calcRes(vidHeight));
    
    // supports multiple poses in an image
-   poseArr.forEach(({ pose }) => {
-      pose && pose.keypoints.forEach(({ position, score }) => {
-         if (score > 0.7) {
+   poseArr.length && poseArr.forEach(({ pose }) => {
+      pose.keypoints.forEach(({ position, score }) => {
+         if (score > minConfidence) {
             circle(position.x*imageScaleFactor, position.y*imageScaleFactor, 10);
          }
       });
+
+      function connectParts(part1, part2) {
+          if (pose[part1].confidence > minConfidence && pose[part2].confidence > minConfidence) {
+             line(pose[part1].x*imageScaleFactor, pose[part1].y*imageScaleFactor, pose[part2].x*imageScaleFactor, pose[part2].y*imageScaleFactor);
+          }
+      }
+
+      // pose skeleton
+      push();
+
+      stroke(255);
+      strokeWeight(5);
+
+      // body outline
+      connectParts('leftShoulder', 'rightShoulder');
+      connectParts('rightShoulder', 'rightHip');
+      connectParts('rightHip', 'leftHip');
+      connectParts('leftHip', 'leftShoulder');
+
+      // left arm
+      connectParts('leftShoulder', 'leftElbow');
+      connectParts('leftElbow', 'leftWrist');
+
+      // right arm
+      connectParts('rightShoulder', 'rightElbow');
+      connectParts('rightElbow', 'rightWrist');
+
+      // left leg
+      connectParts('leftHip', 'leftKnee');
+      connectParts('leftKnee', 'leftAnkle');
+      
+      // right leg
+      connectParts('rightHip', 'rightKnee');
+      connectParts('rightKnee', 'rightAnkle');
+
+      pop();
    });
 }
